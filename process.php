410c1c71520c4f3a257f2321806fbac474b376c1
Manjunath <manjummmvvv18@gmail.com>
2013-08-21 20:23:17 +0530

    added battery tempearture code

--- process.php
+++ process.php
@@ -15,8 +15,8 @@ echo "data from server is::" ;
 $dataarr = (explode(",",$data));
 
 $x= $dataarr;
-$query = "INSERT INTO details (Time,IMEINO,Signal_strength,Area,Network_operator,Phone_Model,Phone_Os_Version,MANUFACTURER,Board,Display) VALUES ('$x[0]','$x[1]','$x[2]','$x[3]',
-'$x[4]','$x[5]','$x[6]','$x[7]','$x[8]','$x[9]')";
+$query = "INSERT INTO details (Time,IMEINO,Signal_strength,Area,Network_operator,Phone_Model,Phone_Os_Version,MANUFACTURER,Board,Display,Battery_Temperature) VALUES ('$x[0]','$x[1]','$x[2]','$x[3]',
+'$x[4]','$x[5]','$x[6]','$x[7]','$x[8]','$x[9]','$x[10]')";
 
 
 $ret_value=mysql_query($query);
