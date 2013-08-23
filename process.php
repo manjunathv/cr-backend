e16d73f0e1298a22874632c82374934ef1571d71
Manjunath <manjummmvvv18@gmail.com>
2013-08-23 20:12:48 +0530

    added location data

--- process.php
+++ process.php
@@ -15,8 +15,8 @@ echo "data from server is::" ;
 $dataarr = (explode(",",$data));
 
 $x= $dataarr;
-$query = "INSERT INTO details (Time,IMEINO,Signal_strength,Area,Network_operator,Phone_Model,Phone_Os_Version,MANUFACTURER,Board,Display,Battery_Temperature,Battery_Health) VALUES ('$x[0]','$x[1]','$x[2]','$x[3]',
-'$x[4]','$x[5]','$x[6]','$x[7]','$x[8]','$x[9]','$x[10]','$x[11]')";
+$query = "INSERT INTO details (Time,IMEINO,Signal_strength,Area,Network_operator,Phone_Model,Phone_Os_Version,MANUFACTURER,Board,Display,Battery_Temperature,Battery_Health,Location) VALUES ('$x[0]','$x[1]','$x[2]','$x[3]',
+'$x[4]','$x[5]','$x[6]','$x[7]','$x[8]','$x[9]','$x[10]','$x[11]','$x[12]')";
 
 
 $ret_value=mysql_query($query);
