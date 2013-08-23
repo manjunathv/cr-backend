fb89ec626eac44c686d02f65a892d8dd43c46f90
Manjunath <manjummmvvv18@gmail.com>
2013-08-23 20:14:32 +0530

    added location data

--- display.php
+++ display.php
@@ -16,6 +16,7 @@ echo" <table border='1' width='100%'>
 <th>DISPLAY</th>
 <th>BATTERY_TEMPERATURE</th>
 <th>BATTERY_HEALTH</th>
+<th>LOCATION</th>
 </tr>";
 
 $result2=mysql_query("SELECT * FROM details ",$mysql) or die("error");
@@ -25,7 +26,7 @@ while ($row=mysql_fetch_array($result2))
 	echo "<tr align='center'>";
 	echo "<td>".$row['Time']."</td>";
 	echo "<td>".$row['IMEINO']."</td><td>".$row['Signal_strength']."</td><td>".$row['Area']."</td>";
-	echo "<td>" . $row['Network_operator'] . "</td><td>" . $row['Phone_Model'] . "</td><td>" . $row['Phone_Os_Version'] . "</td><td> ".$row['MANUFACTURER']."</td><td>".$row['Board']."</td><td>".$row['Display']."</td><td>".$row['Battery_Temperature']."</td><td>".$row['Battery_Health']."</td>" ;
+	echo "<td>" . $row['Network_operator'] . "</td><td>" . $row['Phone_Model'] . "</td><td>" . $row['Phone_Os_Version'] . "</td><td> ".$row['MANUFACTURER']."</td><td>".$row['Board']."</td><td>".$row['Display']."</td><td>".$row['Battery_Temperature']."</td><td>".$row['Battery_Health']."</td><td>".$row['Location']."</td>" ;
 	echo "</tr>" ;	
 }
 echo "</table>"
